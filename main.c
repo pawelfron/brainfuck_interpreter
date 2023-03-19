ed690dfbd3f07ccfc48aa57e7642b115f9119023
pfron <pavelfron4@gmail.com>
2023-03-19 17:17:56 +0100

    added basic file io

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MEMORY_SIZE 1024
 
 int main(void) {
+    unsigned char memory[MEMORY_SIZE];
+    for (size_t i = 0; i < MEMORY_SIZE; i++)
+        memory[i] = 0;
+
+    FILE *file = fopen("program.bf", "rb");
+    if (file == NULL) {
+        printf("Error while opening the brainfuck source file");
+        return 1;
+    }
+
+    char character = fgetc(file);
+    while (!feof(file)) {
+        printf("%c", character);
+        character = fgetc(file);
+    }
+        
+
+    fclose(file);
     return 0;
 }
