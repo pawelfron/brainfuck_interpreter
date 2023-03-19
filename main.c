f9d894365be99599785c1b3a06bb1fe70be47b07
pfron <pavelfron4@gmail.com>
2023-03-19 21:19:25 +0100

    changed the way files are read

--- main.c
+++ main.c
@@ -18,6 +18,48 @@ void closing_jump(size_t pointer, unsigned char *memory, FILE *file); // ]
 // size of the brainfuck memory array
 #define MEMORY_SIZE 1024
 
+#define MOVE_LEFT 0
+#define MOVE_RIGHT 1
+#define INCREMENT 2
+#define DECREMENT 3
+#define INPUT 4
+#define OUTPUT 5
+#define OPEN_LOOP 6
+#define CLOSE_LOOP 7
+
+char *read_source_file(char *name, size_t *command_count) {
+    char buffer[10000];
+    size_t counter = 0;
+    FILE *file = fopen(name, "rb");
+    if (file == NULL) {
+        printf("Error while opening the brainfuck source file");
+        exit(1);
+    }
+
+    char character = fgetc(file);
+    while(!feof(file)) {
+        if (character == '>') buffer[counter] = MOVE_LEFT;
+        else if (character == '<') buffer[counter] = MOVE_RIGHT;
+        else if (character == '+') buffer[counter] = INCREMENT;
+        else if (character == '-') buffer[counter] = DECREMENT;
+        else if (character == ',') buffer[counter] = INPUT;
+        else if (character == '.') buffer[counter] = OUTPUT;
+        else if (character == '[') buffer[counter] = OPEN_LOOP;
+        else if (character == ']') buffer[counter] = CLOSE_LOOP;
+        counter++;
+        character = fgetc(file);
+    }
+
+    fclose(file);
+
+    char *commands = (char *) malloc(counter);
+    for (int i = 0; i < counter; i++)
+        commands[i] = buffer[i];
+
+    *command_count = counter;
+    return commands;
+}
+
 int main(void) {
     /* 
     Some bs to change the behaviour of the terminal.
@@ -36,40 +78,20 @@ int main(void) {
     unsigned char *memory = (unsigned char*) calloc(MEMORY_SIZE, 1);
     size_t pointer = 0;
 
-    FILE *file = fopen("program.bf", "rb");
-    if (file == NULL) {
-        printf("Error while opening the brainfuck source file");
-        return 1;
-    }
+    size_t command_count = 0;
+    char *commands = read_source_file("program.bf", &command_count);
 
-    unsigned char character = fgetc(file);
-    while (!feof(file)) {
-        switch (character) {
-            case '>':
-                increment_pointer(&pointer);
-                break;
-            case '<':
-                decrement_pointer(&pointer);
-                break;
-            case '+':
-                increment_cell(memory + pointer);
-                break;
-            case '-':
-                decrement_cell(memory + pointer);
-                break;
-            case ',':
-                input(memory + pointer);
-                break;
-            case '.':
-                output(memory + pointer);
-                break;
-        }
-        character = fgetc(file);
+    for (int i = 0; i < command_count; i++) {
+        if (commands[i] == MOVE_LEFT) increment_pointer(&pointer);
+        else if (commands[i] == MOVE_RIGHT) decrement_pointer(&pointer);
+        else if (commands[i] == INCREMENT) increment_cell(memory + pointer);
+        else if (commands[i] == DECREMENT) decrement_cell(memory + pointer);
+        else if (commands[i] == INPUT) input(memory + pointer);
+        else if (commands[i] == OUTPUT) output(memory + pointer);
     }
-
-    fclose(file);
 
     free(memory);
+    free(commands);
 
     /* Reversing the changes to the terminal made by the bs above */
     tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
@@ -96,12 +118,12 @@ void decrement_pointer(size_t *pointer) {
 
 void increment_cell(unsigned char *cell) {
     if (*cell == 255) *cell = 0;
-    *cell += 1;
+    else *cell += 1;
 }
 
 void decrement_cell(unsigned char *cell) {
     if (*cell == 0) *cell = 255;
-    *cell -= 1;
+    else *cell -= 1;
 }
 
 void input(unsigned char *cell) { *cell = getchar(); }
