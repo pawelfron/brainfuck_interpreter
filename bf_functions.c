d4385561269db1a0d44ecc49d0715f661ef9c97b
pfron <pavelfron4@gmail.com>
2023-03-19 18:06:32 +0100

    implemented input and output bf functions

--- bf_functions.c
+++ bf_functions.c
@@ -26,16 +26,15 @@ void decrement_cell(size_t pointer, unsigned char **memory) {
     if (*memory[pointer] == 0b00000000) *memory[pointer] = 0b11111111;
     else *memory[pointer]++;
 }
+
+void input(size_t pointer, unsigned char *memory) {
+    memory[pointer] = getchar();
+}
+
+void output(size_t pointer, unsigned char *memory) {
+    putchar(memory[pointer]);
+}
 /*
-
-void input() {
-
-}
-
-void output() {
-
-}
-
 void opening_jump() {
 
 }
