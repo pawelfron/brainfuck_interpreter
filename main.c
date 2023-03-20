4b56a563cef77dabb468228e950c5d17ba7226dc
pfron <pavelfron4@gmail.com>
2023-03-20 17:04:44 +0100

    reformatting, memory safety additions

--- main.c
+++ main.c
@@ -2,15 +2,9 @@
 #include <stdlib.h>
 #include <signal.h>
 
-/* Libraries for the bs below */
+/* Libraries specific to UNIX-like systems */
 #include <termios.h>
 #include <unistd.h>
-/* ---- */
-
-/*
-TODO validate the brackets 
-TODO shorter code inside the brackets handler
-*/
 
 #define MOVE_LEFT 0 // <
 #define MOVE_RIGHT 1 // >
@@ -21,145 +15,122 @@ TODO shorter code inside the brackets handler
 #define OPEN_LOOP 6 // [
 #define CLOSE_LOOP 7 // ]
 
-#define MEMORY_SIZE 1024 // size of the brainfuck memory array
-#define BUFFER_SIZE 10000
+#define MEMORY_SIZE 1024 /* size of the brainfuck memory array */
+#define BUFFER_SIZE 5120 /* size of the buffer for loading the source file */
 
-/* global variables, so that is possible to free the pointers and to reverse the change to terminal inside the signal handler funtion */
+/* global variables, so that is possible to free heap-allocated memory and to restore previous terminal settigns from the signal_handler funtion (which can't take custom arguments) */
 struct termios oldattr, newattr;
 unsigned char *commands = NULL, *memory = NULL; 
 
-unsigned char *read_source_file(char *name, size_t *command_count); /* loading the entier program to memory, without comments */
-void change_terminal_behaviour(); /* changing the behaviour of the terminal */
-void clean(); /* freeing heap-allocated memory and reversing the change made by the above function */
-void signal_handler(int signal_number); /* custom signal handler */
+size_t read_source_file(char *name); /* loads the entire program to memory, without comments, returns the number of instructions */
+void change_terminal_behaviour(); /* changes the behaviour of the terminal */
+void raise_error(char *error_message); /* frees heap-allocated memory, restores the previous terminal settings and exits */
+void signal_handler(int signal_number); /* custom signal handler; necessery due to changes to the default terminal behaviour */
 
 int main(void) {
-    size_t command_counter = 0;
-    commands = read_source_file("program.bf", &command_counter);
-
-    size_t pointer = 0;
-    memory = (unsigned char*) calloc(MEMORY_SIZE, 1);
-
     change_terminal_behaviour();
-
-    /* setting the signal handler; here, not earlier, because the clean function frees memory from *commands and *memory, so they have to be allocated beforehand */
     signal(SIGINT, signal_handler);
+
+    size_t pointer = 0, command_counter = read_source_file("program.bf");
+    memory = (unsigned char*) calloc(MEMORY_SIZE, 1);
+    if (memory == NULL) raise_error("Couldn't allocate memory\n");
 
     for (int i = 0; i < command_counter; i++) {
         if (commands[i] == MOVE_LEFT) {
-            if (pointer == 0) {
-                printf("\nAttempted to move pointer beyond the left bound\n");
-                clean();
-                return 1;
-            }
+            if (pointer == 0) raise_error("Attempted to move pointer beyond the left bound");
             pointer--;
-
         } else if (commands[i] == MOVE_RIGHT) {
-            if (pointer == MEMORY_SIZE - 1) {
-                printf("\nAttempted to move pointer beyond the right bound\n");
-                clean();
-                return 1;
-            }
+            if (pointer == MEMORY_SIZE - 1) raise_error("Attempted to move pointer beyond the right bound");
             pointer++;
-
         } else if (commands[i] == INCREMENT){
             if (memory[pointer] == 255) memory[pointer] = 0;
             else memory[pointer]++;
-
         } else if (commands[i] == DECREMENT) {
             if (memory[pointer] == 0) memory[pointer] = 255;
             else memory[pointer]--;
-
         } else if (commands[i] == INPUT) {
             memory[pointer] = getchar();
-
         } else if (commands[i] == OUTPUT) {
             putchar(memory[pointer]);
-
         } else if (commands[i] == OPEN_LOOP && memory[pointer] == 0) {
-            size_t bracket_counter = 0;
             i++;
-            while (1) {
-                if (commands[i] == CLOSE_LOOP && bracket_counter == 0) break;
-
+            for (size_t bracket_counter = 0; commands[i] != CLOSE_LOOP || bracket_counter != 0; i++) {
                 if (commands[i] == OPEN_LOOP) bracket_counter++;
                 else if (commands[i] == CLOSE_LOOP) bracket_counter--;
-                i++;
             }
-
-        } else if (commands[i] == CLOSE_LOOP && memory[pointer] != 0) {
-            size_t bracket_counter = 0;
+        } else if (commands[i] == CLOSE_LOOP && memory[pointer] != 0) { 
             i--;
-            while (1) {
-                if (commands[i] == OPEN_LOOP && bracket_counter == 0) break;
-
+            for (size_t bracket_counter = 0; commands[i] != OPEN_LOOP || bracket_counter != 0 ; i--) {
                 if (commands[i] == CLOSE_LOOP) bracket_counter++;
                 else if (commands[i] == OPEN_LOOP) bracket_counter--;
-                i--;
             }
         }
     }
 
-    clean();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr); /* restore the previous terminal settings */
+    free(commands);
+    free(memory);
     return 0;
 }
 
-unsigned char *read_source_file(char *name, size_t *command_count) {
-    unsigned char buffer[BUFFER_SIZE];
-    size_t counter = 0;
 
+/* TODO read the source file in 1kb chunks */
+size_t read_source_file(char *name) {
     FILE *file = fopen(name, "rb");
-    if (file == NULL) {
-        printf("Error while opening the brainfuck source file");
-        exit(1);
-    }
+    if (file == NULL) raise_error("Couldn't open the source file");
 
-    unsigned char character = fgetc(file);
+    size_t command_counter, bracket_counter = 0;
+    unsigned char buffer[BUFFER_SIZE], character = fgetc(file);
     while(!feof(file)) {
-        if (character == '<') buffer[counter++] = MOVE_LEFT;
-        else if (character == '>') buffer[counter++] = MOVE_RIGHT;
-        else if (character == '+') buffer[counter++] = INCREMENT;
-        else if (character == '-') buffer[counter++] = DECREMENT;
-        else if (character == ',') buffer[counter++] = INPUT;
-        else if (character == '.') buffer[counter++] = OUTPUT;
-        else if (character == '[') buffer[counter++] = OPEN_LOOP;
-        else if (character == ']') buffer[counter++] = CLOSE_LOOP;
+        if (character == '<') buffer[command_counter++] = MOVE_LEFT;
+        else if (character == '>') buffer[command_counter++] = MOVE_RIGHT;
+        else if (character == '+') buffer[command_counter++] = INCREMENT;
+        else if (character == '-') buffer[command_counter++] = DECREMENT;
+        else if (character == ',') buffer[command_counter++] = INPUT;
+        else if (character == '.') buffer[command_counter++] = OUTPUT;
+        else if (character == '[') {
+            buffer[command_counter++] = OPEN_LOOP;
+            bracket_counter++;
+        } else if (character == ']') {
+            buffer[command_counter++] = CLOSE_LOOP;
+            if (bracket_counter == 0) {
+                fclose(file);
+                raise_error("Square brackets don't match");
+            }
+            bracket_counter--;
+        }
 
         character = fgetc(file);
     }
 
     fclose(file);
 
-    unsigned char *commands = (unsigned char *) malloc(counter);
-    for (int i = 0; i < counter; i++)
+    if (bracket_counter != 0) raise_error("Square brackets don't match");
+
+    commands = (unsigned char *) malloc(command_counter);
+    if (commands == NULL) raise_error("Couldn't allocate memory\n");
+    for (int i = 0; i < command_counter; i++)
         commands[i] = buffer[i];
 
-    *command_count = counter;
-    return commands;
+    return command_counter;
 }
 
 void change_terminal_behaviour() {
-    /* 
-    Some bs to change the behaviour of the terminal.
-    It causes the getchar() function to get just a single character from stdin, without the need to press enter.
-    I have no clue how any of this works, but it works.
-
-    based on ChatGPT answer and someone answering this question on Stack Overflow: https://stackoverflow.com/questions/1798511/how-to-avoid-pressing-enter-with-getchar-for-reading-a-single-character-only
-    */
+    /*  The code below causes the getchar() function to get just a single character from stdin, without the need to press enter. Based on ChatGPT's answer and this answer on Stack Overflow: https://stackoverflow.com/a/1798833 */
     tcgetattr(STDIN_FILENO, &oldattr);
     newattr = oldattr;
     newattr.c_lflag &= ~(ICANON);
     tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
 }
 
-void clean() {
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+void raise_error(char *error_message) {
+    printf("\nError: %s\n", error_message);
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr); /* restore the previous terminal settings */
     free(commands);
     free(memory);
+    _exit(1);
 }
 
 void signal_handler(int signal_number) {
-    printf("\nProgram interrupted\n");
-    clean();
-    exit(1);
+    raise_error("Program interrupted");
 }
